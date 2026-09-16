b1b52ba6c0daa88f9ec0585ec796c55c2f769a20
taladnumberoneram2-netizen <taladnumberoneram2@gmail.com>
2026-09-16 11:43:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Public frontend configuration.
 // Replace the value below with your deployed Google Apps Script Web App URL.
 window.DASHBOARD_CONFIG = {
-  API_URL: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE?action=getSheetData"
+  API_URL: "https://script.google.com/macros/s/AKfycbyZ8oSYC5eEe-b6Z467TG0RYcsaxi_6Eyrd5sCeab0RMPOfMA2Z7QeUeua0A7ClJ0sJsw/exec?action=getSheetData"
 };
